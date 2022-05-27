95466b9ff77aea199b3a6b49f9fa2e60ec917db9
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-05-27 20:18:43 -0300

    Conclusão da classe aluno
    Classe aluno foi concluído

--- Diario/Model/Aluno.cs
+++ Diario/Model/Aluno.cs
@@ -3,12 +3,63 @@ namespace Model
 {
     public class Aluno
     {
-        private string NomeAluno;
-        public string nomeAluno
+        private int id_Aluno;
+        private int id_Turma;
+        private string nomeAluno;
+        private string enderecoAluno;
+        private string cidadeAluno;
+        private string telefoneAluno;
+        private string email;
+
+
+
+        public int Id_Aluno
         {
-            get { return NomeAluno; }
-            set {NomeAluno = value;}
+            get { return id_Aluno; }
+            set { id_Aluno = value; }
         }
+
+
+        public int Id_Turma
+        {
+            get { return id_Turma; }
+            set { id_Turma = value; }
+        }
+
+
+        public string NomeAluno
+        {
+            get { return nomeAluno; }
+            set {nomeAluno = value;}
+        }
+
+        public string EnderecoAluno
+        {
+            get { return enderecoAluno; }
+            set { enderecoAluno = value; }
+        }
+
+
+        public string CidadeAluno
+        {
+            get { return cidadeAluno; }
+            set { cidadeAluno = value; }
+        }
+
+
+        public string TelefoneAluno
+        {
+            get { return telefoneAluno; }
+            set { telefoneAluno = value; }
+        }
+
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
 
     }
 }
