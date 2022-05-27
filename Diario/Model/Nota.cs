24fb6ae7383b155dd1ca4c54abbdfc521a82c425
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-05-27 20:31:49 -0300

    Atualizado classe nota

--- Diario/Model/Nota.cs
+++ Diario/Model/Nota.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Model
+﻿namespace Model
 {
-    class Nota
+    public class Nota
     {
+        private int id_Nota;
+        private int id_Aluno;
+        private int id_Turma;
+        private bool apontamento;
+
+        public int Id_Nota
+        {
+            get { return id_Nota; }
+            set { id_Nota = value; }
+        }
+
+        public int Id_Aluno
+        {
+            get { return id_Aluno; }
+            set { id_Aluno = value; }
+        }
+
+
+        public int Id_Turma
+        {
+            get { return id_Turma; }
+            set { id_Turma = value; }
+        }
+
+
+        public bool Apontamento
+        {
+            get { return apontamento; }
+            set { apontamento = value; }
+        }
+
+
     }
 }
