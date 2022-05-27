8265824f13130bc3c46a69ddfe0480bc1f993ae0
djaphantom <djacksonfelix71@gmail.com>
2022-05-27 20:57:26 -0300

    Terminando as classes professor e Frequencia
    terminando de criar e conectar ao banco de dados as classes professor e a Classe frequencia.

---
+++ Diario/Model/Frequencia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Frequencia
+    {
+        public int Id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+        private int frequencia;
+
+        public int DescricaoFrequencia
+        {
+            get { return frequencia; }
+            set { frequencia = value; }
+        }
+    }
+}
--- Diario/Model/Professor.cs
+++ Diario/Model/Professor.cs
@@ -8,5 +8,54 @@ namespace Model
 {
     public class Professor
     {
+        private int Id;
+        private string NomeProfessor;
+        private string EnderecoProfessor;
+        private string CidadeProfessor;
+        private string CPF_Professor;
+        private string Email;
+        private string Telefone;
+
+        public int id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string nomeProfessor
+        {
+            get { return nomeProfessor; }
+            set { nomeProfessor = value; }
+        }
+        public string enderecoProfessor
+        {
+            get { return enderecoProfessor; }
+            set { enderecoProfessor = value; }
+        }
+
+        public String cidadeProfessor
+        {
+
+            get { return cidadeProfessor; }
+            set { cidadeProfessor = value; }
+        }
+
+        public string cpf_Professor
+        {
+            get { return cpf_Professor; }
+            set { cpf_Professor = value; }
+        }
+        public string email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+
     }
 }
