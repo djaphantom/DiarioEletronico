d1a2857aeab880e10ac155ec5b3ac688a58a10cf
Bruno Alencar Moreira <90804306+BMoreiraa@users.noreply.github.com>
2022-05-30 21:28:47 -0300

    atualização
    atualização do banco de dados

--- BANDO DE DADOS DIARIO.sql
+++ BANDO DE DADOS DIARIO.sql
@@ -17,16 +17,18 @@ CREATE TABLE Aluno
 	Id int PRIMARY KEY IDENTITY (1,1)NOT NULL,
 	NomeAluno varchar(100) NULL,
 	DataDeNascimento varchar(50)NULL,
-	EnderecoAluno varchar(100) NULL,
-	CidadeAluno varchar(100) NULL,
 	TelefoneResponsavel varchar(50) NULL,
+	cpf varchar(100)null,
 	Email varchar(100) NULL,
-	Id_Turma int NOT NULL,
 	NomeDoResponsavel varchar(100)NULL,
 	sexo varchar(50)NULL,
+	CidadeAluno varchar(100) NULL,
+	UF varchar(50)null,
+	EnderecoAluno varchar(100) NULL,
 	setor varchar(100)NULL,
 	numero INT NULL,
-	cep varchar(50)
+	cep varchar(50),
+	senha varchar(50)not null
  )
 GO
 
@@ -65,7 +67,7 @@ CREATE TABLE Nota(
 	Id int PRIMARY KEY IDENTITY (1,1) NOT NULL,
 	Id_Aluno int NULL,
 	Id_Turma int NULL,
-	Apontamento FLOAT NULL,
+	notaAluno FLOAT NULL,
 )
 GO
 /*-----------------------------------------------------------------------------------------------*/
@@ -86,11 +88,17 @@ GO
 CREATE TABLE Professor(
 	Id int PRIMARY KEY IDENTITY (1,1) NOT NULL,
 	NomeProfessor varchar(100) NULL,
-	EnderecoProfessor varchar(100) NULL,
-	CidadeProfessor varchar(100) NULL,
 	CPF_Professor varchar(100) NULL,
 	Email varchar(100) NULL,
 	Telefone varchar(50) NULL,
+	DataDeNascimento varchar(50)NULL,
+	sexo varchar(50)NULL,
+	CidadeProfessor varchar(100) NULL,
+	UF varchar(50)null,
+	EnderecoProfessor varchar(100) NULL,
+	setor varchar(100)NULL,
+	cep varchar(50)null,
+	senha varchar(50)not null
 )
 GO
 /*-----------------------------------------------------------------------------------------------*/
@@ -98,6 +106,14 @@ CREATE TABLE Turma(
 	Id int PRIMARY KEY IDENTITY (1,1) NOT NULL,
 	Id_Aluno int NOT NULL,
 	Periodo varchar(100) NULL,
+)
+GO
+/*-----------------------------------------------------------------------------------------------*/
+CREATE TABLE Secretaria(
+	Id int PRIMARY KEY IDENTITY (1,1) NOT NULL,
+	senha varchar(50)not null,
+	NomeUsuario varchar(100) NULL,
+	Id_Diario int
 )
 GO
 /*-----------------------------------------------------------------------------------------------*/
@@ -110,24 +126,36 @@ select*from Ocorrencia
 select*from PlanoDeAula
 select*from Professor
 select*from Turma
+Select*from Secretaria
 /*-----------------------------------------------------------------------------------------------*/
 					/*area de altera as tablelas se for preciso*/
-/*Alter table Ocorrencia
-add Descricao varchar(300)
+/*Alter table Secretaria
+add Id_Diario int
 
-Alter table Turma
-drop column Id__Aluno*/
+Alter table Secretaria
+drop column NomeUsurio*/
 
 /*-----------------------------------------------------------------------------------------------*/
-CREATE PROCEDURE SP_InserirUsuario
-	@ID int OUTPUT,
-	@NomeUsuario VARCHAR(150),
-	@Senha VARCHAR(100),
-	@Ativo BIT
+CREATE PROCEDURE SP_InserirAluno
+	@Id int ,
+	@NomeAluno varchar(100) ,
+	@DataDeNascimento varchar(50),
+	@TelefoneResponsavel varchar(50) ,
+	@cpf varchar(100),
+	@Email varchar(100) ,
+	@NomeDoResponsavel varchar(100),
+	@sexo varchar(50),
+	@CidadeAluno varchar(100) ,
+	@UF varchar(50),
+	@EnderecoAluno varchar(100) ,
+	@setor varchar(100),
+	@numero INT ,
+	@cep varchar(50),
+	@senha varchar(50)
 AS
-	INSERT INTO USUARIO(Ativo, NomeUsuario, Senha)
-	Values(@Ativo, @NomeUsuario, @Senha)
-	SET @ID = (SELECT @@IDENTITY)
+	INSERT INTO Aluno(Id, NomeAluno, DataDeNascimento,TelefoneResponsavel,cpf,Email,NomeDoResponsavel,sexo,CidadeAluno,UF,EnderecoAluno,setor,numero,cep,senha)
+	Values(@Id,@NomeAluno,@DataDeNascimento,@TelefoneResponsavel,@cpf,@Email,@NomeDoResponsavel,@sexo,@CidadeAluno,@UF,@EnderecoAluno,@setor,@numero,@cep,@senha)
+	SET @Id = (SELECT @@IDENTITY)
 	--SELECT @@IDENTITY
 GO
 /*-----------------------------------------------------------------------------------------------*/
