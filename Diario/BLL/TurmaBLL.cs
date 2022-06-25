9f4783b47f5f4b725706834c28e2d2c6034e8678
djaphantom <90804343+djaphantom@users.noreply.github.com>
2022-06-25 18:45:25 -0300

    classes turma
    treminando as  classes turma

--- Diario/BLL/TurmaBLL.cs
+++ Diario/BLL/TurmaBLL.cs
@@ -9,30 +9,30 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    /*public class TurmaBLL
+    public class TurmaBLL
     {
         public void Inserir(Turma _turma)
         {
-            //TurmaDAL turmaDAL = new TurmaDAL();
-            //turmaDAL.Inserir(_turma);
+            TurmaDAL turmaDAL = new TurmaDAL();
+            turmaDAL.Inserir(_turma);
         }
         public DataTable Buscar(string _filtro)
         {
-            //TurmaDAL turmaDAL = new TurmaDAL();
-            //return TurmaDAL.Buscar(_filtro);
-            throw new NotImplementedException();
+            TurmaDAL turmaDAL = new TurmaDAL();
+            return turmaDAL.Buscar(_filtro);
+           
         }
         public Turma Alterar(Turma _turma)
         {
-            //AgentePedagogicoDAL turmaDAL = new AgentePedagogicoDAL();
-            //return turmaDAL.Alterar(_turma);
+            TurmaDAL turmaDAL = new TurmaDAL();
+            return turmaDAL.Alterar(_turma);
             throw new NotImplementedException();
         }
         public void Excluir(int _id)
         {
-            //TurmaDAL turmaDAL = new TurmaDAL();
-            //turmaDAL.Excluir(_id);
+            TurmaDAL turmaDAL = new TurmaDAL();
+            turmaDAL.Excluir(_id);
         }
 
-    }*/
+    }
 }
