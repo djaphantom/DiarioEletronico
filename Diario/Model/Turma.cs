36777d091590ceb7ec4007181cef9cad5f967eb8
djaphantom <90804343+djaphantom@users.noreply.github.com>
2022-06-24 21:43:22 -0300

    Concluindo a model turma
    concluir e modificar a model Turma

--- Diario/Model/Turma.cs
+++ Diario/Model/Turma.cs
@@ -8,5 +8,18 @@ namespace Model
 {
     public class Turma
     {
+        private int id;
+        private string periodo;
+
+        public string Periodo
+        {
+            get { return periodo; }
+            set { periodo = value; }
+        }
+        public int Id
+        {
+            get { return id; }  
+            set { id = value; } 
+        }
     }
 }
