bf87f792c9545aaaeea3e1b69a2eab48fb84fe61
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-06-24 21:36:28 -0300

    Classe UF BLL pronta

---
+++ Diario/BLL/UFBLL.cs
@@ -0,0 +1,36 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class UFBLL
+    {
+        public UF Inserir(UF _uF)
+        {
+            UfDAL uF = new UfDAL();
+            return uF.Inserir(_uF);
+
+        }
+        public DataTable Buscar(string _filtro)
+        {
+            UfDAL uF = new UfDAL();
+            return uF.Buscar(_filtro);
+        }
+        public UF Alterar(UF _uF)
+        {
+            UfDAL uF = new UfDAL();
+            return uF.Alterar(_uF);
+        }
+        public void Excuir(int _id)
+        {
+            UfDAL cidadeDAL = new UfDAL();
+            cidadeDAL.Excluir(_id);
+        }
+    }
+}
