be41a96b62bcae4785e7483aa0e26e5b01dc81b6
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-06-24 21:16:27 -0300

    Classe diário BLL pronta

--- Diario/BLL/DiarioEletronicoBLL.cs
+++ Diario/BLL/DiarioEletronicoBLL.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL;
+using Model;
+using System.Data;
 
 namespace BLL
 {
-    class DiarioEletronicoBLL
+    public class DiarioEletronicoBLL
     {
+        public DiarioEletronico Inserir(DiarioEletronico _diarioEletronico)
+        {
+
+            DiarioEletronicoDAL diarioEletronicoDAL = new DiarioEletronicoDAL();
+            return diarioEletronicoDAL.Inserir(_diarioEletronico);
+        }
+        public DataTable Buscar(string _filtro)
+        {
+            DiarioEletronicoDAL diarioEletronicoDAL = new DiarioEletronicoDAL();
+            return diarioEletronicoDAL.Buscar(_filtro);
+
+        }
+        public DiarioEletronico Alterar(DiarioEletronico _diarioEletronico)
+        {
+            DiarioEletronicoDAL diarioEletronicoDAL = new DiarioEletronicoDAL();
+            return diarioEletronicoDAL.Alterar(_diarioEletronico);
+        }
+        public void Excluir(int _Id)
+        {
+            DiarioEletronicoDAL diarioEletronicoDAL = new DiarioEletronicoDAL();
+            diarioEletronicoDAL.Excluir(_Id);
+        }
     }
 }
