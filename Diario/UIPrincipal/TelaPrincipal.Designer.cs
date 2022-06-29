3e59fe2d7f63100c884aa93bb21fce517ab44cdc
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-06-28 21:01:33 -0300

    teste

--- Diario/UIPrincipal/TelaPrincipal.Designer.cs
+++ Diario/UIPrincipal/TelaPrincipal.Designer.cs
@@ -29,129 +29,69 @@ namespace UIPrincipal
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPrincipal));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPlanodeAula = new System.Windows.Forms.Button();
+            this.buttonLancamentodeNota = new System.Windows.Forms.Button();
+            this.buttonLancamentodeFrequência = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // buttonPlanodeAula
             // 
-            this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(877, 99);
-            this.panel1.TabIndex = 0;
+            this.buttonPlanodeAula.Location = new System.Drawing.Point(12, 83);
+            this.buttonPlanodeAula.Name = "buttonPlanodeAula";
+            this.buttonPlanodeAula.Size = new System.Drawing.Size(123, 44);
+            this.buttonPlanodeAula.TabIndex = 0;
+            this.buttonPlanodeAula.Text = "Plano de aula";
+            this.buttonPlanodeAula.UseVisualStyleBackColor = true;
             // 
-            // panel2
+            // buttonLancamentodeNota
             // 
-            this.panel2.BackColor = System.Drawing.Color.Teal;
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 99);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(131, 425);
-            this.panel2.TabIndex = 1;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.buttonLancamentodeNota.Location = new System.Drawing.Point(12, 159);
+            this.buttonLancamentodeNota.Name = "buttonLancamentodeNota";
+            this.buttonLancamentodeNota.Size = new System.Drawing.Size(123, 44);
+            this.buttonLancamentodeNota.TabIndex = 1;
+            this.buttonLancamentodeNota.Text = "Lançamento de nota";
+            this.buttonLancamentodeNota.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // buttonLancamentodeFrequência
             // 
-            this.button3.BackColor = System.Drawing.Color.Transparent;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(0, 118);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(131, 53);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Century", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.PaleTurquoise;
-            this.button2.Location = new System.Drawing.Point(0, 59);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 53);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Cadastro de professor";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Century", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.PaleTurquoise;
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 53);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Cadastro de aluno";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(357, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 57);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Menu";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.buttonLancamentodeFrequência.Location = new System.Drawing.Point(166, 83);
+            this.buttonLancamentodeFrequência.Name = "buttonLancamentodeFrequência";
+            this.buttonLancamentodeFrequência.Size = new System.Drawing.Size(123, 44);
+            this.buttonLancamentodeFrequência.TabIndex = 2;
+            this.buttonLancamentodeFrequência.Text = "Lançamento de frequência";
+            this.buttonLancamentodeFrequência.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(834, 3);
+            this.button4.Location = new System.Drawing.Point(12, 266);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(43, 33);
-            this.button4.TabIndex = 1;
+            this.button4.Size = new System.Drawing.Size(123, 35);
+            this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // Form1
+            // TelaPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(877, 524);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Form1";
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonLancamentodeFrequência);
+            this.Controls.Add(this.buttonLancamentodeNota);
+            this.Controls.Add(this.buttonPlanodeAula);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "TelaPrincipal";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPlanodeAula;
+        private System.Windows.Forms.Button buttonLancamentodeNota;
+        private System.Windows.Forms.Button buttonLancamentodeFrequência;
         private System.Windows.Forms.Button button4;
     }
 }
