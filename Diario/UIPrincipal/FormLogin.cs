1188a8f06c6cbead4b8e7f2389a301737d57fca3
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-08-16 21:39:15 -0300

    teste

--- Diario/UIPrincipal/FormLogin.cs
+++ Diario/UIPrincipal/FormLogin.cs
@@ -52,7 +52,8 @@ namespace UIPrincipal
                 nome = ((DataRowView)AgenteBindingSource.Current).Row["NomeUsuario"].ToString();
                 senha = ((DataRowView)AgenteBindingSource.Current).Row["Senha"].ToString();
 
-                if (nome == textBoxUsuario.Text && Infra.Criptografia.verificarHash(textBoxSenha.Text, senha))
+                // if (nome == textBoxUsuario.Text && Infra.Criptografia.verificarHash(textBoxSenha.Text, senha))
+                if (nome == textBoxUsuario.Text && senha == textBoxSenha.Text)
                 {
                     Logou = true;
                     this.Visible = !this.Visible;
